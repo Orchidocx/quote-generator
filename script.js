086fce0102f087962e4eda54e6856f40236c6cdc
Jacky Li <ilikemacaronii@gmail.com>
2020-08-11 13:22:16 -0700

    using dadjokesapi

--- script.js
+++ script.js
@@ -1,21 +1,29 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+// const quoteAuthor = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+
 // Get Quote From API
 async function getQuote() {
   const apiUrl = 'https://icanhazdadjoke.com';
   try {
-    const response = await fetch(apiUrl, 
-      {method:'GET',
-       mode: 'cors',
-       headers: {
-         'Content-Type': 'application/json'
-       }
-      });
+    const response = await fetch(apiUrl, {headers: {'Accept': 'application/json'}});
     const data = await response.json();
-    console.log(data);
+    const {joke} = data;
+    if(joke.length > 100) {
+      quoteText.classList.add('long-quote');
+    } else {
+      quoteText.classList.remove('long-quote');
+    }
+    quoteText.innerText = joke;
   } catch(err) {
-    // getQuote();
-    console.log('No quote, oops!', err);
+    getQuote();
   }
 }
+
+
+
 
 // On Load
 getQuote();
